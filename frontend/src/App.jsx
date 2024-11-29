802fc3f2f1a8185669f6b76465cf4c0eae1b850e
KRojowsky <rojowsky@poczta.fm>
2024-11-29 14:29:32 +0100

    adding TopSellers section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Stores from './Components/Stores/Stores'
+import TopSellers from './Components/TopSellers/TopSellers'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
@@ -9,7 +10,8 @@ const App = () => {
   return (
     <div>
       <Navbar/>
-      <Stores />
+      <Stores/>
+      <TopSellers/>
     </div>
   )
 }
---
+++ frontend/src/Components/TopSellers/TopSellers.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./TopSellers.scss";
+import placeholder from "../../assets/placeholder.jpg";
+
+
+const TopSellers = () => {
+
+  return (
+    <div className="section top-sellers">
+        <div className="container">
+            <h2 className="title">Lorem Ipsum</h2>
+        </div>
+    </div>
+  );
+};
+
+export default TopSellers;
